7a4c1fd865f10b040d8c2b86a2f95662c46a7bdb
amir <amirtaki623@gmail.com>
2025-08-25 19:41:15 +0330

    plugin at js

---
+++ src/MyPlugin.js
@@ -0,0 +1,13 @@
+(function ($){
+    $.fn.faradars = function () {
+        this.css({color: "blue", opacity : .3})
+    };
+
+
+    $.fn.changeColor = function() {
+        this.css({color: "white", backgroundColor: "blue", opacity: 1});
+    };
+
+    
+
+})(jQuery);
